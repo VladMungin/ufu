b41d342b52d8f2303e9c74449cc1a0e482a1295d
vlad mungin <vlad.mungin@yandex.ru>
2025-03-11 13:52:09 +0300

    add locale

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,8 +1,9 @@
-import { LocalizationProvider } from '@mui/x-date-pickers'
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { Provider } from 'jotai'
 import { CookiesProvider } from 'react-cookie'
+import { IntlProvider } from 'react-intl'
+import { CustomProvider } from 'rsuite'
+import { ruRU } from 'rsuite/esm/locales'
 import AppRoutes from '../../Routes/Routes'
 import BG from '../BG/BG'
 import Header from '../Header/Header'
@@ -11,19 +12,21 @@ function App() {
   const queryClient = new QueryClient()
 
   return (
-    <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <CookiesProvider defaultSetOptions={{ path: '/' }}>
-        <Provider>
-          <QueryClientProvider client={queryClient}>
-            <div className="relative mx-auto max-w-[1920px]">
-              <Header />
-              <BG />
-              <AppRoutes />
-            </div>
-          </QueryClientProvider>
-        </Provider>
-      </CookiesProvider>
-    </LocalizationProvider>
+    <IntlProvider locale="ru">
+      <CustomProvider locale={ruRU}>
+        <CookiesProvider defaultSetOptions={{ path: '/' }}>
+          <Provider>
+            <QueryClientProvider client={queryClient}>
+              <div className="relative mx-auto max-w-[1920px]">
+                <Header />
+                <BG />
+                <AppRoutes />
+              </div>
+            </QueryClientProvider>
+          </Provider>
+        </CookiesProvider>
+      </CustomProvider>
+    </IntlProvider>
   )
 }
 
