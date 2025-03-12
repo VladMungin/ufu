d929a1c48367184836899fee1504195cfd73acdc
vlad mungin <vlad.mungin@yandex.ru>
2025-03-12 15:40:14 +0300

    add

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import topLevelAwait from 'vite-plugin-top-level-await'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  optimizeDeps: {
-    include: ['pdfjs-dist'],
+  plugins: [react(), topLevelAwait()],
+  esbuild: {
+    target: 'es2020', // или 'esnext'
   },
 })
